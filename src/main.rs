2c0548bd061fcfeac5a2db252fedc480b2bf335c
0xull <mrikehchukwuka@gmail.com>
2025-09-18 20:22:39 +0100

    refactor: the child process now replaces its process image with that of /bin/sh
    As well, the update setups up a new hostname for the new PID namespace

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,63 @@
-use nix::sched::{clone, CloneFlags};
+use std::ffi::CString;
+
+use nix::sched::{CloneFlags, clone};
 use nix::sys::signal::Signal;
 use nix::sys::wait::{self, WaitStatus};
+use nix::unistd::execv;
 
 const STACK_SIZE: usize = 1024 * 1024;
 
 fn child_main() -> isize {
-    println!("Child process: I'm alive people!");
-    0
+    println!("Child process: Setting up environment...");
+
+    unsafe {
+        let hostname =
+            CString::new("foundry-container").expect("failed to create C nul-terminated string");
+        let res = nix::libc::sethostname(hostname.as_ptr(), hostname.as_bytes().len());
+        if res != 0 {
+            eprintln!("Child process: sethostname failed.");
+            return 1;
+        }
+    };
+    println!("Child process: hostname of process changed.");
+
+    let shell_path = CString::new("/bin/sh").expect("failed to created C nul-terminated string");
+    let shell_args = [shell_path.as_c_str()];
+
+    match execv(&shell_path, &shell_args) {
+        Err(err) => {
+            eprintln!("Child process: execv failed with error: {err}");
+            return 1;
+        }
+    }
 }
 
 fn main() {
     let mut stack = [0_u8; STACK_SIZE];
-    
+
     println!("Parent process: about to create a child process");
-    
+
     let flags = CloneFlags::CLONE_NEWPID | CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_NEWUTS;
     let sigchld = Some(Signal::SIGCHLD as i32);
-        
-    let clone_result = unsafe {
-        clone(Box::new(|| child_main()), &mut stack, flags, sigchld)
-    };
-    
+
+    let clone_result = unsafe { clone(Box::new(|| child_main()), &mut stack, flags, sigchld) };
+
     match clone_result {
-        Ok(pid) => {
-            match wait::waitpid(pid, None) {
-                Ok(WaitStatus::Exited(child_pid, exit_code)) => {
-                    println!("Parent process: child process {} exited with code {}", child_pid, exit_code)
-                }
-                Ok(status) => {
-                    println!("Parent process: child process exited with unexpected status {:?}", status)
-                },
-                Err(err) => eprintln!("Parent process: waitpid failed with error: {err}")
+        Ok(pid) => match wait::waitpid(pid, None) {
+            Ok(WaitStatus::Exited(child_pid, exit_code)) => {
+                println!(
+                    "Parent process: child process {} exited with code {}",
+                    child_pid, exit_code
+                )
             }
+            Ok(status) => {
+                println!(
+                    "Parent process: child process exited with unexpected status {:?}",
+                    status
+                )
+            }
+            Err(err) => eprintln!("Parent process: waitpid failed with error: {err}"),
         },
-        Err(err) => eprintln!("Parent process: clone failed with error: {err}")
+        Err(err) => eprintln!("Parent process: clone failed with error: {err}"),
     }
 }
