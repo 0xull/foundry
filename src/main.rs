8389f16b23d9e120a351584b5b56231ea8ef1d71
0xull <mrikehchukwuka@gmail.com>
2025-09-18 15:45:12 +0100

    refactor: still implementing the initial setup logic
    Now, we can create a new child process inside an empty isolated namespaces (PID, UTS and Mount).

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
+use nix::sched::{clone, CloneFlags};
+use nix::sys::signal::Signal;
+use nix::sys::wait::{self, WaitStatus};
+
 const STACK_SIZE: usize = 1024 * 1024;
 
-fn _child_process() -> isize {
+fn child_main() -> isize {
     println!("Child process: I'm alive people!");
     0
 }
 
 fn main() {
-    let _stack = [0_u8; STACK_SIZE];
+    let mut stack = [0_u8; STACK_SIZE];
     
     println!("Parent process: about to create a child process");
+    
+    let flags = CloneFlags::CLONE_NEWPID | CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_NEWUTS;
+    let sigchld = Some(Signal::SIGCHLD as i32);
+        
+    let clone_result = unsafe {
+        clone(Box::new(|| child_main()), &mut stack, flags, sigchld)
+    };
+    
+    match clone_result {
+        Ok(pid) => {
+            match wait::waitpid(pid, None) {
+                Ok(WaitStatus::Exited(child_pid, exit_code)) => {
+                    println!("Parent process: child process {} exited with code {}", child_pid, exit_code)
+                }
+                Ok(status) => {
+                    println!("Parent process: child process exited with unexpected status {:?}", status)
+                },
+                Err(err) => eprintln!("Parent process: waitpid failed with error: {err}")
+            }
+        },
+        Err(err) => eprintln!("Parent process: clone failed with error: {err}")
+    }
 }
