801db808a7374cb8a4985791600a242b96313cae
0xull <mrikehchukwuka@gmail.com>
2025-10-10 16:43:19 +0100

    feat(mount-namespace): this completes the mount namespace isolation
    This combines the PID and Mount namespaces to isolate the container's view of the overarching filesystem, hences limiting its accesses

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,27 @@ use std::io::BufReader;
 use std::path::Path;
 use std::{env, fs};
 
-use nix::mount::{mount, umount, umount2, MntFlags, MsFlags};
+use nix::mount::{MntFlags, MsFlags, mount, umount2};
 use nix::sched::{CloneFlags, clone};
 use nix::sys::signal::Signal;
 use nix::sys::wait::{self, WaitStatus};
-use nix::unistd::{chdir, execv, pivot_root};
+use nix::unistd::{chdir, execvp, pivot_root};
 
 use serde::Deserialize;
 
 const STACK_SIZE: usize = 1024 * 1024;
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct OCIProcess {
     args: Vec<String>,
     env: Vec<String>,
-    cwd: String,
+    cwd: String
 }
 
 #[derive(Deserialize, Debug)]
 struct OCIRoot {
     path: String,
-    readonly: bool,
 }
 
 #[derive(Deserialize, Debug)]
@@ -40,57 +40,112 @@ fn read_from_file(file: fs::File) -> Result<OCIConfig, Box<dyn Error>> {
     Ok(config)
 }
 
-fn child_main() -> isize {
-    println!("Child process: Setting up environment...");
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 || args[1] != "run" {
+        return Err("Usage: ./foundry run <path-to-bundle>".into());
+    }
 
-    unsafe {
-        let hostname =
-            CString::new("foundry-container").expect("failed to create C nul-terminated string");
-        let res = nix::libc::sethostname(hostname.as_ptr(), hostname.as_bytes().len());
-        if res != 0 {
-            eprintln!("Child process: sethostname failed.");
-            return 1;
+    let bundle_path = Path::new(&args[2]);
+    let config_path = bundle_path.join("config.json");
+
+    let oci_config = read_from_file(fs::File::open(&config_path)?)?;
+    let rootfs_path = bundle_path.join(&oci_config.root.path);
+
+    let mut stack = [0_u8; STACK_SIZE];
+    let flags = CloneFlags::CLONE_NEWPID | CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_NEWUTS;
+    let sigchld = Some(Signal::SIGCHLD as i32);
+
+    let child_closure = || -> isize {
+        let hostname = CString::new("foundry-container").unwrap();
+
+        if unsafe { nix::libc::sethostname(hostname.as_ptr(), hostname.as_bytes().len()) } != 0 {
+            eprintln!("Child process: sethostname failed");
+            return -1;
         }
-        println!("Child process: hostname of process changed.");
 
-        let new_root = "./oci-bundle/rootfs";
         mount(
-            Some(new_root),
-            new_root.as_bytes(),
+            None::<&Path>,
+            "/",
+            None::<&Path>,
+            MsFlags::MS_REC | MsFlags::MS_PRIVATE,
+            None::<&Path>,
+        )
+        .expect("Failed to make root mount private");
+
+        mount(
+            Some(&rootfs_path),
+            &rootfs_path,
             None::<&Path>,
             MsFlags::MS_BIND,
             None::<&Path>,
         )
-        .expect("failed to bind mount the new rootfs");
-        pivot_root(".", ".").expect("failed to stack mount the new rootfs");
-        chdir(new_root).expect("failed to change directory");
-        umount2(".", MntFlags::MNT_DETACH).expect("failed to unmount old rootfs");
+        .expect("Failed to bind mount the new rootfs onto itself");
+
+        let old_root_put_path = rootfs_path.join(".old_root");
+        fs::create_dir(&old_root_put_path).expect("failed to create .old_root directory for pivot");
+
+        pivot_root(&rootfs_path, &old_root_put_path)
+            .expect("pivot_root failed. Ensure new root is a mount point.");
+        chdir("/").expect("Failed to chdir into new root directory");
+        umount2("/.old_root", MntFlags::MNT_DETACH).expect("Failed to unmount old root.");
+
+        fs::create_dir_all("/proc").expect("Failed to create /proc");
+        fs::create_dir_all("/sys").expect("Failed to create /sys");
+        fs::create_dir_all("/dev").expect("Failed to create /dev");
+        
+        mount(
+            Some("proc"),
+            "/proc",
+            Some("proc"),
+            MsFlags::empty(),
+            None::<&Path>,
+        )
+        .unwrap();
+        mount(
+            Some("sysfs"),
+            "/sys",
+            Some("sysfs"),
+            MsFlags::empty(),
+            None::<&Path>,
+        )
+        .unwrap();
+        mount(
+            Some("tmpfs"),
+            "/dev",
+            Some("tmpfs"),
+            MsFlags::empty(),
+            None::<&Path>,
+        )
+        .unwrap();
+        
+        for var in &oci_config.process.env {
+            let parts: Vec<&str> = var.splitn(2, "=").collect();
+            if parts.len() == 2 {
+                unsafe {env::set_var(parts[0], parts[1])};
+            }
+        }
+        
+        let cwd = Path::new(&oci_config.process.cwd);
+        chdir(cwd).expect("Failed to chdir to OCI cwd");
+
+        let command = &oci_config.process.args[0];
+        let args = &oci_config.process.args;
+        let c_command = CString::new(command.as_bytes()).unwrap();
+        let c_args: Vec<CString> = args
+            .iter()
+            .map(|s| CString::new(s.as_bytes()).unwrap())
+            .collect();
+        
+        match execvp(&c_command, &c_args) {
+            Err(err) => {
+                eprintln!("Child process: execv failed with error: {err}");
+                return 1;
+            }
+        }
     };
 
-    let shell_path = CString::new("/bin/sh").expect("failed to created C nul-terminated string");
-    let shell_args = [shell_path.as_c_str()];
-
-    match execv(&shell_path, &shell_args) {
-        Err(err) => {
-            eprintln!("Child process: execv failed with error: {err}");
-            return 1;
-        }
-    }
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    let oci_config = read_from_file(fs::File::open(&args[1])?)?;
-    println!("{oci_config:#?}");
-
-    let mut stack = [0_u8; STACK_SIZE];
-
-    println!("Parent process: about to create a child process");
-
-    let flags = CloneFlags::CLONE_NEWPID | CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_NEWUTS;
-    let sigchld = Some(Signal::SIGCHLD as i32);
-
-    let clone_result = unsafe { clone(Box::new(|| child_main()), &mut stack, flags, sigchld) };
+    let clone_result = unsafe { clone(Box::new(child_closure), &mut stack, flags, sigchld) };
 
     match clone_result {
         Ok(pid) => match wait::waitpid(pid, None) {
